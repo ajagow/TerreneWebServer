61522de391b0bdb12d425fd3f42d8f4f5406a356
ajagow <budgiejag@gmail.com>
2020-03-29 16:39:24 -0400

    allow everyone access lmaoooo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connection = require("./database");
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
